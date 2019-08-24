de88e01de014527342df333571b005e273f83079
Hernan Llano <3d.hernan@gmail.com>
2019-08-24 14:26:34 +0100

    adding a path tokenizer module that will be used to create the final path to the houdini version installed in the computer

---
+++ python/path_tokenizer.py
@@ -0,0 +1,4 @@
+# TODO Create a dictionary for each software containing a set of tokens to build
+# the final real path to the application, that will be read by the
+# houdini_set_env.py file so we can point to the right houdini version installed
+# in the computer
