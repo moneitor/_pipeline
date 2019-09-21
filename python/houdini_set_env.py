2bbc4137c74bd1e0585b141294cb964cefe77a07
Hernan Llano <3d.hernan@gmail.com>
2019-09-21 18:00:29 +0100

    changes in the houdini environment vars

--- python/houdini_set_env.py
+++ python/houdini_set_env.py
@@ -6,8 +6,11 @@ def houdini_env(project_path, hou_main_path, hou_doc_path, render_path, project_
     e = os.environ
     pwd = os.path.dirname(os.path.abspath(__file__))
     
+    #pathArnold = r"~/htoa/htoa-4.1.1_rf2c4e45_houdini-17.5.258/htoa-4.1.1_rf2c4e45_houdini-17.5.258"
+  
+    
     hfs = hou_main_path
-    e['HH'] = os.pathsep.join([os.path.join(hfs, 'houdini'), os.path.expanduser(hou_doc_path)])
+    e['HH'] = os.pathsep.join([os.path.expanduser(hou_doc_path), os.path.join(hfs, 'houdini')])
     e['HOUDINI_PATH'] = ';'.join([os.path.join(pwd, 'houdini'), e['HH'], e.get('HOUDINI_PATH', '')])
     e['HOUDINI_TOOLBAR_PATH'] = ';'.join([e.get('HOUDINI_PATH', ''), e['HH'], os.path.join(pwd, 'houdini')])
     e['JOB'] = project_path + "\\02_work\\fx\\01_houdini"      
