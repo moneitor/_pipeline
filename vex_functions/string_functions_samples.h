7593d9bd45405809b491ec5c1b2fb29c3f3afa57
Hernan Llano <33673448+moneitor@users.noreply.github.com>
2019-12-09 16:10:08 +0000

    Create string_functions_samples.h

---
+++ vex_functions/string_functions_samples.h
@@ -0,0 +1,20 @@
+//i@test = endswith(s@foo, "g");
+//i@test = startswith(s@foo, "test");
+//i@test = find(s@foo, "stro");
+//i@test = match("test", s@foo);
+
+//s@new = concat(s@foo, "_something");
+//append(s@foo, "_somehing");
+
+//string myarray[] = {"this", "is", "a", "test"};
+//s@new = join(myarray, "_");
+//s[]@array = split(s@new, "_");
+
+//string path = chs("path");
+//string dir, file;
+//splitpath(path, dir, file);
+//s@dir = dir;
+//s@file = file;
+
+//s@new = lstrip("   test");
+//s@new = rstrip("test   ");
