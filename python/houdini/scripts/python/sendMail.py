45f61c5f1e452b4b4fb1533c18b5790b759472f0
Hernan <3d.hernan@gmail.com>
2021-02-06 01:17:24 +0000

    fixed a few lines

--- python/houdini/scripts/python/sendMail.py
+++ python/houdini/scripts/python/sendMail.py
@@ -1,11 +1,11 @@
 import smtplib as s
 
 def sendMailto (email , password , nodeName):
-    mymail = '3d.hernan@gmail.com'
+    mymail = 'youremail@gmail.com'
     conn = s.SMTP("smtp.gmail.com" , 587)
     conn.ehlo()
     conn.starttls()
-    conn.login(mymail , 'moneitor1053')
+    conn.login(mymail , 'yourpassword')
     conn.sendmail(mymail , mymail , 'Subject: The render of node, is done. ')
 
     conn.quit() 
